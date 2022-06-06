ac15e1e68a2fdf7ac1cb453a21b04a488bd9262d
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-06 18:19:42 +0530

    [Elavarasu] TC3.2-Custom Exception for Empty

--- MoodTesting/UnitTest1.cs
+++ MoodTesting/UnitTest1.cs
@@ -16,5 +16,19 @@ namespace MoodTesting
                 Assert.AreEqual("Message should not be Null", ex.Message);
             }   
         }
+        [Test]
+        public void GivenEmptyAsInput_AnalyseMood_ThrowEmptyException()
+        {
+            try
+            {
+                string moodInput = "";
+                MoodAnalyser.Analyser analyser = new MoodAnalyser.Analyser(moodInput);
+                string output = analyser.Mood();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Message should not be Empty", ex.Message);
+            }
+        }
     }
 }
