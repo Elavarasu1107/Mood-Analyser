6204fa201b72eb5caf9aef7324d441b380d5dd6b
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-06 11:25:20 +0530

    [Elavarasu] TC1.2-Check Any Mood and Return Happy

--- MoodTesting/UnitTest1.cs
+++ MoodTesting/UnitTest1.cs
@@ -3,11 +3,18 @@ namespace MoodTesting
     public class Tests
     {
         [Test]
-        public void GetInput_AnalyseMood_ReturnResult()
+        public void GetInput_AnalyseMood_ReturnSad()
         {
             MoodAnalyser.Analyser analyser = new MoodAnalyser.Analyser();
             string moodInput = "I am in Sad Mood";
             Assert.AreEqual("Sad", analyser.Mood(moodInput));
+        }
+        [Test]
+        public void GetInput_AnalyseAnyMood_ReturnHappy()
+        {
+            MoodAnalyser.Analyser analyser = new MoodAnalyser.Analyser();
+            string moodInput = "I am in Any Mood";
+            Assert.AreEqual("Happy", analyser.Mood(moodInput));
         }
     }
 }
