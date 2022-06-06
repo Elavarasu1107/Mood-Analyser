9f60d6fad455fd6612c324b6621536564e658f08
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-06 13:16:30 +0530

    [Elavarasu] UC1-Checking Happy Mood using Constructor

--- MoodTesting/UnitTest1.cs
+++ MoodTesting/UnitTest1.cs
@@ -9,5 +9,12 @@ namespace MoodTesting
             MoodAnalyser.Analyser analyser = new MoodAnalyser.Analyser(moodInput);
             Assert.AreEqual("Sad", analyser.Mood());
         }
+        [Test]
+        public void GetInputAsHappy_AnalyseMood_ReturnHappy()
+        {
+            string moodInput = "I am in Any Mood";
+            MoodAnalyser.Analyser analyser = new MoodAnalyser.Analyser(moodInput);
+            Assert.AreEqual("Happy", analyser.Mood());
+        }
     }
 }
