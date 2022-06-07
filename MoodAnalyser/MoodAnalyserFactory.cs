8e58133860eb344699037164d7b2d629b8f980fe
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-07 11:22:13 +0530

    [Elavarasu] UC4-Create Mood Analyser using Reflection

---
+++ MoodAnalyser/MoodAnalyserFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserFactory
+    {
+        public static object MoodObject(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
+            if(result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type analyserType = executing.GetType(className);
+                    return Activator.CreateInstance(analyserType);
+                }
+                catch(ArgumentNullException)
+                {
+                    throw new CustomException(CustomException.ExceptionType.NO_SUCH_METHOD, "Class Not Found");
+                }
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.NO_SUCH_METHOD, "Constructor Not Found");
+            }
+        }
+    }
+}
